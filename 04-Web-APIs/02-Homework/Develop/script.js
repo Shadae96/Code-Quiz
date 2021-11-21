16ff6bc28aef68628efed8d8df96f596e6840d7f
Shadae Brown <shadaeamoy@gmail.com>
2021-11-20 19:27:51 -0500

    updated JS

--- 04-Web-APIs/02-Homework/Develop/script.js
+++ 04-Web-APIs/02-Homework/Develop/script.js
@@ -1,69 +1,20 @@
-const startButton= document.querySelector (".start-btn")
-const starterPage= document.querySelector(".starter")
-const questionContainer= document.querySelector(".option-container")
+const startButton = document.getElementById('start-btn')
+const startQuiz= document.querySelector(".starter")
+const questionContainer= document.getElementById("container")
 let shuffledQuestions, currectQuestionIndex
 let questionElement = document.getElementById ("question")
 let answerButtonsElement = document.getElementById("answer-btn")
-
-
-
-let question = [
-    {ask: 'Commonly used data types DO NOT include:',
-    alternatives: [
-        {text:'strings', correct: false},
-        {text:'booleans', correct: false},
-        {tet:'alerts', correct: true},
-        {text:'numbers', correct:false},
-
-        ]
-
-    },
-    {
-        ask: 'Commonly used data types DO NOT include:',
-        alternatives: [
-            {text:'strings', correct: false},
-            {text:'booleans', correct: false},
-            {tet:'alerts', correct: true},
-            {text:'numbers', correct:false},
-            
-            ]
-    
-        }  ,
-        {
-            ask: 'Commonly used data types DO NOT include:',
-            alternatives: [
-                {text:'strings', correct: false},
-                {text:'booleans', correct: false},
-                {tet:'alerts', correct: true},
-                {text:'numbers', correct:false},
-                
-                ]
-        
-            } ,
-            {
-                ask: 'Commonly used data types DO NOT include:',
-                alternatives: [
-                    {text:'strings', correct: false},
-                    {text:'booleans', correct: false},
-                    {tet:'alerts', correct: true},
-                    {text:'numbers', correct:false},
-                    
-                    ]
-            
-                } 
-];
-
-
+var timerEl = document.getElementById("time");
 var timerCount;
 var timer;
 
-startButton.addEventListener('click',startGame)
+startButton.addEventListener('click', startGame)
 
 function startGame() {
 console.log('started')
-startButton.classList.add('hide');
-questionContainer.classList.remove('hide');
-shuffledQuestions = question.sort(()=> Math.random() -.5);
+startButton.classList.add('hide')
+questionContainer.classList.remove('hide')
+shuffledQuestions = questions.sort(()=> Math.random() -.5);
 currentQuestionIndex=0;
 startTimer();
 setNextQuestion();
@@ -76,13 +27,19 @@ function setNextQuestion (){showQuestion(shuffledQuestions[currectQuestionIndex]
 }
 
 function showQuestion(question){
-    questionElement.innerText = question.ask
-question.alternatives.forEach(alternatives => {
-    const button = document.createElement('button')
-    button.innerText = alternatives.text
-    button.classList.add("button")
+questionElement.innerText = question.question
+// question.alternatives.forEach(alternatives => {
+//     const button = document.createElement('button')
+//     button.innerText = alternatives.text
+//     button.classList.add("button")
+//     if (alternatives.correct) {
+//         button.dataset.correct = alternatives.correct
+//     }
+//     button.addEventListener('click', selectAnswer)
+//     answerButtonsElement.appendChild(button)
     
-})}
+// })
+}
 
 // function selectAnswer () {
 
@@ -93,29 +50,77 @@ question.alternatives.forEach(alternatives => {
 // }
 
 
-function startTimer() {
-  timer = setInterval(function() {
-    timerCount--;
-    timerElement.textContent = timerCount;
-    if (timerCount >= 0) {
-      // Tests if win condition is met
-      if (isWin && timerCount > 0) {
-        // Clears interval and stops timer
-        clearInterval(timer);
-        winGame();
-      }
-    }
-    // Tests if time has run out
-    if (timerCount === 0) {
-      // Clears interval
-      clearInterval(timer);
-      loseGame();
-    }
-  }, 1000);
+function startTimer () { 
+    var secondsLeft = 75;
+    var countDown = setInterval(() => {
+    secondsLeft --;
+    timerEl.textContent = "Time Left: " + secondsLeft;
+        if (secondsLeft <= 0) {
+            clearInterval(countDown);
+        }
+    }, 1000);
 }
+
+
 
 
     
 // function showFianlScore () {
 
 // }
+let questions = [
+    {ask: 'Commonly used data types DO NOT include:',
+    alternatives: [
+        {text:'strings', correct: false},
+        {text:'booleans', correct: false},
+        {tet:'alerts', correct: true},
+        {text:'numbers', correct:false},
+
+        ]
+
+    },
+    {
+        ask: 'The condition in an if / else statement is enclosed within ____.',
+        alternatives: [
+            {text:'quotess', correct: false},
+            {text:'curly brackets', correct: false},
+            {tet:'parentheses', correct: true},
+            {text:'square brackets', correct:false},
+            
+            ]
+    
+        }  ,
+        {
+            ask: 'Arrays in JavaScript can be used to store ____.',
+            alternatives: [
+                {text:'numbers and strings', correct: false},
+                {text:'other arrays', correct: false},
+                {tet:'booleans', correct: false},
+                {text:'all of the above', correct:true},
+                
+                ]
+        
+            } ,
+            {
+                ask: 'String values must be enclosed within ____ when being assigned to variables.',
+                alternatives: [
+                    {text:'commas', correct: false},
+                    {text:'curly brackets', correct: false},
+                    {tet:'quotes', correct: true},
+                    {text:'parentheses', correct:false},
+                    
+                    ]
+            
+                }, 
+                {
+                    ask: 'A very useful tool used during development and debugging for printing content to the debugger is',
+                    alternatives: [
+                        {text:'JavaScript', correct: false},
+                        {text:'terminal', correct: false},
+                        {tet:'for loops', correct: false},
+                        {text:'console.log', correct:true},
+                
+                        ]
+                
+                    } 
+];
